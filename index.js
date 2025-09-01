c84b22e4f6e5d95664b1ecdcd649bcb4b14a5a5b
Sanjeewa <sanjeewaxy@gmail.com>
2025-09-01 11:09:42 +0400

    routes handling

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ const app = express();
 const PORT = 3000;
 
 // simple route
-
 app.get("/", (req, res) => {
   res.send("Hello World! in express ch");
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", (req, res) => { 
   res.send("Hello World! in express ch about");
 });
 
 app.get("/contact", (req, res) => {
   res.send("Hello World! in express ch contact");
+});
+
+// routes with parameters
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Hello World! in express ch user id: ${id}`);
+});
+
+// routes with query parameters
+// /user?branch=it
+app.get("/user", (req, res) => {
+  const keywords = req.query.branch;
+  res.send(`Hello World! in express ch user id: ${keywords}`);
 });
 
 app.listen(PORT, () => {
