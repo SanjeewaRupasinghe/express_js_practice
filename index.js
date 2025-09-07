3841d9a6e87fca5b2c8449911ef9269149834179
sanjeewa <sanjeewaxy@gmail.com>
2025-09-07 18:56:12 +0400

    file uploads in index js file

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from "express";
 import router from "./route.js";
-const app = express();
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: function (req, file, cb) {
+    cb(null,file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const app = express();
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024*1024,
+  },
+});
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.single("image"));
+
+app.post("/form", (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("Form submitted");
+});
+
+// static
 app.use(express.static("public"));
 
 // EJS make as view engine
@@ -13,7 +37,7 @@ app.get("/", (req, res) => {
 });
 
 // app level middleware
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   console.log("App level Middleware");
   next();
 });
@@ -23,17 +47,17 @@ app.get("/", (req, res) => {
   res.send("Hello World! in express ch");
 });
 
-app.get("/error", (req, res)=>{
+app.get("/error", (req, res) => {
   throw new Error("Error");
-})
+});
 
 app.use("/", router);
 
 // error handling middleware
-app.use((err, req, res, next)=>{
-  console.log(err) ;
+app.use((err, req, res, next) => {
+  console.log(err);
   res.send("Error");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
